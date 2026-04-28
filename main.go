e9c5c15c9bf08335ca0325ac3f01cf7dbf6af57a
deltrexgg <deltrexgg@gmail.com>
2026-04-29 02:18:28 +0530

    better cors

--- main.go
+++ main.go
@@ -23,16 +23,14 @@ func LoggingMiddleware(next http.Handler) http.Handler {
 		)
 	})
 }
-
 func CORSMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-
 		w.Header().Set("Access-Control-Allow-Origin", "*")
 		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
-		w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
+		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
 
-		if r.Method == http.MethodOptions {
-			w.WriteHeader(http.StatusOK)
+		if r.Method == "OPTIONS" {
+			w.WriteHeader(204)
 			return
 		}
 
