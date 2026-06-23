90f2b38091d3353ac5f90b1c996bc7518f893d6b
Hari Nandan <deltrexgg@gmail.com>
2026-06-23 15:49:37 +0530

    gemini to flash 2.5

--- internals/ai/files.go
+++ internals/ai/files.go
@@ -246,7 +246,7 @@ func GeminiFileStructure(content string)(string,error){
 
 
 	url :=
-	"https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash:generateContent?key="+apiKey
+"https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash-lite:generateContent?key="+apiKey
 
 
 
@@ -300,7 +300,7 @@ User:
 
 			"temperature":0.3,
 
-			"maxOutputTokens":400,
+			"maxOutputTokens":200,
 
 		},
 
